2ebd361ec9fb3a750a2b1d413446dfe79403e240
shunji_y <syunjimtb715@gmail.com>
2025-08-13 17:07:06 +0900

    add 問合わせ画面

---
+++ my-react-app/src/features/inquiry/InquiryPage.jsx
@@ -0,0 +1,14 @@
+// src/features/home/HomePage.jsx
+import React, { useState } from 'react'
+
+function InquiryPage() {
+
+    
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>toiawase</h1>
+    </div>
+  )
+}
+
+export default InquiryPage
